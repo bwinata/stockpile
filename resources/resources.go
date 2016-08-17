db08fd4f2ef8de794ffcb4e7f8f81e4a5d896c43
Barry Winata <barry.winata@yahoo.com>
2016-08-17 09:52:52 -0700

    Get better memory stats from buffers/cache readings

--- resources/resources.go
+++ resources/resources.go
@@ -1,10 +1,9 @@
 package resources
 
-
 const (
   CPU_USAGE_PERCENTAGE = "top -b -n2 -d 0.75 | grep \"Cpu(s)\" | tail -n 1 | awk '{print $2 + $4}'"
-  MEMORY_USAGE_MB = "free -m | grep \"Mem:\" | awk '{print $3}'"
-  MEMORY_USAGE_PERCENT = "free -m | grep \"Mem:\" | awk '{print ($3 / $2) * 100}'"
+  MEMORY_USAGE_MB = "free -m | grep \"buffers/cache:\" | awk '{print $3}'"
+  MEMORY_USAGE_PERCENT = "free -m | grep \"buffers/cache:\" | awk '{print ($3 / ($3 + $4)) * 100}'"
   SWAP_USAGE_MB = "free -m | grep \"Swap:\" | awk '{print $3}'"
   // SWAP_USAGE_PERCENT = "free -m | grep \"Swap:\" | awk '{print ($3 / $2) * 100}'"
 )
